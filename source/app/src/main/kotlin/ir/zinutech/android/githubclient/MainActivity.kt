29bc0461ed08481e3ba1e20d0949e73bf9ca6327
MohammadReza Najafi <mrnj92@gmail.com>
2018-11-23 09:48:48 +0330

    added gradle dependencies

--- source/app/src/main/kotlin/ir/zinutech/android/githubclient/MainActivity.kt
+++ source/app/src/main/kotlin/ir/zinutech/android/githubclient/MainActivity.kt
@@ -1,6 +1,6 @@
 package ir.zinutech.android.githubclient
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
